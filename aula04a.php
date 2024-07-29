e02a2457abb114d032f3cec382e345498b5f58b7
siqueirabianca1 <169085660+siqueirabianca1@users.noreply.github.com>
2024-07-29 18:19:59 -0300

    Add files via upload

---
+++ aula04a.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Doc</title>
+    <style>
+        table{
+            border-collapse: collapse;
+        }
+        td{
+            border: 1px solid black; 
+        }
+
+    </style>
+</head>
+<body>
+    
+
+<?php
+echo "<h1> Salada de fruta</h1>";
+
+
+echo "<h1> Tabela de 1 a 10 em php</php>";
+echo "<table><tr>";
+for ($x=1; $x<=10;$x++){
+echo "<td>$x</td>";
+
+}
+echo "</tr></table>";
+
+
+
+
+
+?>
+    
+</body>
+</html>
